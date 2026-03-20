73916f9dfc324abc5aa9753961450577306c6069
Antonio Coppola <antonio.coppola@tum.de>
2026-03-20 11:32:37 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -13,10 +12,10 @@ export default defineConfig(({ mode }) => ({
     },
   },
   base: "/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
